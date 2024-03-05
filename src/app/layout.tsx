78d9b295d8ac6a498d3d27c7ac3070605c46e0e4
synergy-gate <synergygate.develop@gmail.com>
2024-03-06 00:26:57 +0900

    lang change jp => ja

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='jp'>
+    <html lang='ja'>
       <body className={noteSansJP.className}>{children}</body>
     </html>
   );
